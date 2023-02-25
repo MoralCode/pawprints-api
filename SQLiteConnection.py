9b31fae37dee8a3c14fe6c5058d8edf415e9e1af
led9628 <led9628@rit.edu>
2023-02-25 16:43:00 -0500

    SQLite Changes

--- SQLiteConnection.py
+++ SQLiteConnection.py
@@ -1,19 +1,21 @@
-import sqlalchemy as db
+from sqlalchemy import create_engine
+from Tables import mapper_registry
 
 def main():
     try:
         # Create a database engine
-        engine = db.create_engine('sqlite:///my_database.db')
+        engine = create_engine('sqlite:///my_database.db')
 
         # Connect to the database
         connection = engine.connect()
 
         #Add actual functionality from other files here (potentially a driver class)
+        mapper_registry.metadata.create_all(engine)
 
-    except Exception at e:
+    except Exception as e:
 
         print(e)
-        Print("Connection Failed")
+        print("Connection Failed")
     
     finally:
         connection.close()
