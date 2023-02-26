5957127c6ce314bbd66f97d49e33f163166c30d1
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-26 10:23:41 -0500

    some of these changes probably fix the alembic package path thing

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ setup(
     name="wtfrit_storage_schema",
     version="0.1.1",
     packages=find_packages(),
-    install_requires=['alembic', 'sqlalchemy']
+    package_dir={'': '.'},
+    package_data={'wtfrit_storage_schema': ['migrations/**/*']},
+    include_package_data=True,
+    install_requires=['alembic', 'sqlalchemy'],
 )
