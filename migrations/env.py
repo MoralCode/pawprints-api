208042808f2ea4f06c7aac238ad00bfbef090331
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-25 19:42:25 -0500

    in theory set up alembic

--- migrations/env.py
+++ migrations/env.py
@@ -18,7 +18,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from Tables import mapper_registry
+target_metadata = mapper_registry.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
