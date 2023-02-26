25e622ed709666bfbda45e3ea3628c14bab96075
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-26 05:12:37 -0500

    fix engine binding

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Database():
 	
 	def __init__(self):
 		self.engine = create_engine('sqlite:///my_database.db')
-		self.session = sessionmaker(bind=engine)()
+		self.session = sessionmaker(bind=self.engine)()
 
 	def initialize(self):
 		#Add actual functionality from other files here (potentially a driver class)
