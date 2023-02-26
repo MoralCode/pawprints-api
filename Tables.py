b5363f33de2e7799ae10fa59cff1b324c7e87120
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-26 04:37:43 -0500

    Revert "reformat schema"
    This reverts commit 3a23f6188ecebed9ee8a5a9558c004acd400bc55.

--- Tables.py
+++ Tables.py
@@ -5,19 +5,26 @@ from sqlalchemy import Integer
 from sqlalchemy import String, Text, Date
 from sqlalchemy import Table
 from sqlalchemy.orm import registry
-from datetime import datetime
+
 
 mapper_registry = registry()
 
 @mapper_registry.mapped
 @dataclass
 class Vibes:
-	__tablename__ = "vibes"
+	__table__ = Table(
+		"vibes",
+		mapper_registry.metadata,
+		Column("id", Integer, primary_key=True),
+		Column("title", Text),
+		Column("contents", Text, nullable=False),
+		Column("upvotes", Integer),
+		Column("sentiment", Integer),
+		Column("source_url", String, nullable=False),
+		Column("last_updated", Date(), nullable=False)
+	)
+	identifier: int
+	title: str
+	contents: str
+	upvotes: int
 	
-	id: int = Column("id", Integer, primary_key=True),
-	title: str = Column("title", Text),
-	contents: str = Column("contents", Text, nullable=False),
-	upvotes: int = Column("upvotes", Integer),
-	sentiment: int =  Column("sentiment", Integer),
-	source_url: str = Column("source_url", String, nullable=False),
-	last_updated: datetime = Column("last_updated", Date(), nullable=False)
