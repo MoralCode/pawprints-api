ddffaabcccbd6e291890beedb0a82bd7e6e87deb
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-02-26 04:59:05 -0500

    stamp database with alembic version on creation

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ class Database():
 
 	def initialize(self):
 		#Add actual functionality from other files here (potentially a driver class)
-        mapper_registry.metadata.create_all(self.engine)
+		# Check if the table has been created
+		table_exists_before = Vibes.name in metadata.tables
+		mapper_registry.metadata.create_all(self.engine)
+		table_exists_after = Vibes.name in metadata.tables
+
+		if not table_exists_before and table_exists_after:
+			# new tables created, version stamp the DB
+			# load the Alembic configuration and generate the
+			# version table, "stamping" it with the most recent rev:
+			from alembic.config import Config
+			from alembic import command
+			alembic_cfg = Config("./alembic.ini")
+			command.stamp(alembic_cfg, "head")
 	
 	
