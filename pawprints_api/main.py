10d831fa1f1154ce1ac573a7e755a0d50ebcc3f3
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-03-05 20:39:45 -0500

    remove auth stuff

--- pawprints_api/main.py
+++ pawprints_api/main.py
@@ -5,15 +5,12 @@ WEBSOCKET_URL = 'wss://pawprints.rit.edu/ws/'
 # WEBSOCKET_URL = 'ws://localhost:8000/ws/'
 
 class PawPrints:
-    def __init__(self, endpoint_url=WEBSOCKET_URL, auth_token=None):
+    def __init__(self, endpoint_url=WEBSOCKET_URL):
         self.endpoint_url = endpoint_url
-        self.auth_token = auth_token
         self.websocket = None
 
     async def connect(self):
         self.websocket = await websockets.connect(self.endpoint_url)
-        if self.auth_token is not None:
-            await self.websocket.send(json.dumps({'auth': self.auth_token}))
 
     async def disconnect(self):
         await self.websocket.close()
