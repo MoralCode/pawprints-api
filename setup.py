6535a337a79c3efe556d2784030002c5f31f6ebf
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-03-05 21:44:40 -0500

    it installs now, maybe

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup, find_packages
 setup(
     name="pawprints_api",
     version="0.1.0",
-    packages=find_packages(),
-    package_dir={'': '.'},
-    package_data={'pawprints_api': '*'},
-    include_package_data=True,
+    packages=find_packages(),#['pawprints_api'],
     install_requires=['websockets'],
 )
