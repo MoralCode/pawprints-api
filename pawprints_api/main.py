1df677ccbdc3cb331538ff9af9d392af0cece33d
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2023-03-05 20:38:24 -0500

    get started with help from ChatGPT

--- pawprints_api/main.py
+++ pawprints_api/main.py
@@ -1,4 +1,44 @@
+import json
+import websockets
 
+WEBSOCKET_URL = 'wss://pawprints.rit.edu/ws/'
+# WEBSOCKET_URL = 'ws://localhost:8000/ws/'
 
-class PawPrints():
-	pass
+class PawPrints:
+    def __init__(self, endpoint_url=WEBSOCKET_URL, auth_token=None):
+        self.endpoint_url = endpoint_url
+        self.auth_token = auth_token
+        self.websocket = None
+
+    async def connect(self):
+        self.websocket = await websockets.connect(self.endpoint_url)
+        if self.auth_token is not None:
+            await self.websocket.send(json.dumps({'auth': self.auth_token}))
+
+    async def disconnect(self):
+        await self.websocket.close()
+
+    async def send_request(self, request):
+        await self.websocket.send(json.dumps(request))
+        response = await self.websocket.recv()
+        return json.loads(response)
+
+    async def get_ticker(self, symbol):
+        request = {
+            'method': 'ticker',
+            'params': {
+                'symbol': symbol
+            }
+        }
+        return await self.send_request(request)
+
+    async def get_order_book(self, symbol, limit):
+        request = {
+            'method': 'orderbook',
+            'params': {
+                'symbol': symbol,
+                'limit': limit
+            }
+        }
+        return await self.send_request(request)
+
